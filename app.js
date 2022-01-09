575dd9379efb7b8c0c4ddebf848e0a7e265e7965
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-09 20:11:20 +0530

    get random color

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   // get random number between 0 - 3
-  const randomNumber = 2;
+  const randomNumber = getRandomNuber();
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
+
+function getRandomNuber() {
+  return Math.floor(Math.random() * colors.length);
+}
